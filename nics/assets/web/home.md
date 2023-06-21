e119b446a2346b9d8f653bdbb3c8393c1023e8c4
Nicholas Valentinus <nvfastplease@gmail.com>
2023-06-21 10:30:12 -0700

    added some assets/ stuff, testing 0.5.0 (_compile cmd)

---
+++ nics/assets/web/home.md
@@ -0,0 +1,19 @@
+---
+permalink: /
+layout: main
+title: Home
+---
+
+## Welcome to My Jekyll Homepage!
+
+This is the content of my homepage.
+
+
+- hello world!
+
+    - wow
+
+
+```sh
+pip install nics
+```
--- nics/compile/__init__.py
+++ nics/compile/__init__.py
@@ -3,18 +3,21 @@ import os
 from mykit.kit.utils import printer
 
 
-def run(input, output):
+def run(docs, target):
     """
     ## Params
-    - `input`:
-    - `output`:
+    - `docs`: the docs/ bundle
+    - `target`: the branch
     """
 
-    printer(f'DEBUG: input: {repr(input)}.')
-    printer(f'DEBUG: output: {repr(output)}.')
+    printer(f'DEBUG: docs: {repr(docs)}.')
+    printer(f'DEBUG: target: {repr(target)}.')
 
-    printer(f'DEBUG: os.path.isdir(input): {os.path.isdir(input)}.')
-    printer(f'DEBUG: os.path.isdir(output): {os.path.isdir(output)}.')
+    printer(f'DEBUG: os.path.isdir(docs): {os.path.isdir(docs)}.')
+    printer(f'DEBUG: os.path.isdir(target): {os.path.isdir(target)}.')
 
-    printer(f'DEBUG: os.listdir(input): {os.listdir(input)}.')
-    printer(f'DEBUG: os.listdir(output): {os.listdir(output)}.')
+    printer(f'DEBUG: os.listdir(docs): {os.listdir(docs)}.')
+    printer(f'DEBUG: os.listdir(target): {os.listdir(target)}.')
+
+    for i in os.walk(docs):
+        print(i)
