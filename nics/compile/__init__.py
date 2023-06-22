fabb605265e4a6d0bbbd341b12128ac15e26d76e
Nicholas Valentinus <nvfastplease@gmail.com>
2023-06-22 03:52:41 -0700

    updated nics folder structure. 0.8.0 testing: testing _compile cmd (header recusion engine)

--- nics/compile/__init__.py
+++ nics/compile/__init__.py
@@ -4,8 +4,8 @@ from mykit.kit.keycrate import KeyCrate
 from mykit.kit.utils import printer
 
 
-def header_writer(docs) -> str:
-    
+def header_writer(container) -> str:
+
     header = (
         '<header>'
 
@@ -17,12 +17,27 @@ def header_writer(docs) -> str:
                 '<div class="main" id="_root__nav-div">'
     )
 
-    for pth, dirs, files in os.walk(docs):
+    ## <build the nested divs recursively>
 
-        printer(f'DEBUG: pth: {pth}')
-        printer(f'DEBUG: dirs: {dirs}')
-        printer(f'DEBUG: files: {files}')
-        printer(f'DEBUG: ---------')
+    def recursion(pth):
+
+        printer(f'DEBUG: pth: {repr(pth)}')
+
+        for i in os.listdir(pth):
+
+            printer(f'DEBUG: {repr(i)}')
+            
+            full_pth = os.path.join(pth, i)
+            if os.path.isdir(full_pth):
+                recursion(full_pth)
+        
+        printer(f'DEBUG: ------------')
+        
+        return 'x'
+
+    header += recursion(container)
+    
+    ## </build the nested divs recursively>
 
     header += (
                 '</div>'
@@ -35,29 +50,32 @@ def header_writer(docs) -> str:
     return header
 
 
-def run(docs, target):
+def run(container, target):
     """
     ## Params
-    - `docs`: the docs/ bundle
+    - `container`: the nics folder bundle
     - `target`: the branch
     """
 
-    printer(f'DEBUG: docs: {repr(docs)}.')
+    printer(f'DEBUG: container: {repr(container)}.')
     printer(f'DEBUG: target: {repr(target)}.')
 
-    printer(f'DEBUG: os.path.isdir(docs): {os.path.isdir(docs)}.')
+    printer(f'DEBUG: os.path.isdir(container): {os.path.isdir(container)}.')
     printer(f'DEBUG: os.path.isdir(target): {os.path.isdir(target)}.')
 
-    printer(f'DEBUG: os.listdir(docs): {os.listdir(docs)}.')
+    printer(f'DEBUG: os.listdir(container): {os.listdir(container)}.')
     printer(f'DEBUG: os.listdir(target): {os.listdir(target)}.')
 
     settings = KeyCrate(
-        os.path.join(docs, '_nics', 'settings.txt'),
+        os.path.join(container, '_nics', 'settings.txt'),
         key_is_var=True, eval_value=True
     )
     
     printer(f'DEBUG: settings.export(): {settings.export()}')
 
-    header = header_writer(docs)
+    header = header_writer(container)
 
     printer(f'DEBUG: header: {header}')
+
+    
+    printer(f'INFO: start copying assets..')
