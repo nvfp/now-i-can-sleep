2c9c48c2425ff4805d25345632e70217f306de16
Nicholas Valentinus <nvfastplease@gmail.com>
2023-06-28 10:41:16 -0700

    0.23.2: debugging: nics/assets/template/_layouts can't be accessed in gh VM

--- nics/compile/copying_template.py
+++ nics/compile/copying_template.py
@@ -6,6 +6,10 @@ from ..constants import ASSETS_DIR_PTH
 
 def copying_template(dock):
 
+    x = os.path.join(ASSETS_DIR_PTH, 'template', '_layouts')
+    print(x)
+    print(os.path.isdir(x))
+
     shutil.copytree(
         os.path.join(ASSETS_DIR_PTH, 'template', '_layouts'),
         os.path.join(dock, '_layouts')
