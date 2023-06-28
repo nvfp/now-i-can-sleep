f490249349cdde736b4d9e771f3d142a9ce4e776
Nicholas Valentinus <nvfastplease@gmail.com>
2023-06-28 10:51:23 -0700

    0.23.4 still debugging

--- nics/compile/copying_template.py
+++ nics/compile/copying_template.py
@@ -1,16 +1,20 @@
 import os
 import shutil
 
-from ..constants import ASSETS_DIR_PTH
+from ..constants import ASSETS_DIR_PTH, DIST_DIR_PTH
 
 
 def copying_template(dock):
 
     x = os.path.join(ASSETS_DIR_PTH, 'template', '_layouts')
-    print(x)
-    print(os.path.isdir(x))
-    print(ASSETS_DIR_PTH)
-    print(os.listdir(ASSETS_DIR_PTH))
+    print(123, x)
+    print(345, os.path.isdir(x))
+    print('ASSETS_DIR_PTH: ', ASSETS_DIR_PTH)
+    print('DIST_DIR_PTH: ', DIST_DIR_PTH)
+    print('os.path.isdir(ASSETS_DIR_PTH): ', os.path.isdir(ASSETS_DIR_PTH))
+    print('os.path.isdir(DIST_DIR_PTH): ', os.path.isdir(DIST_DIR_PTH))
+    print('os.listdir(DIST_DIR_PTH): ', os.listdir(DIST_DIR_PTH))
+    print('os.listdir(ASSETS_DIR_PTH): ', os.listdir(ASSETS_DIR_PTH))
 
     shutil.copytree(
         os.path.join(ASSETS_DIR_PTH, 'template', '_layouts'),
