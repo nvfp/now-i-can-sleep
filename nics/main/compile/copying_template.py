05094bfec24fac5026c7ed9195e1dd439328f987
Nicholas Valentinus <nvfastplease@gmail.com>
2023-06-29 13:31:26 -0700

    tidying copying_template.py

--- nics/main/compile/copying_template.py
+++ nics/main/compile/copying_template.py
@@ -1,32 +1,32 @@
 import os
 import shutil
 
+from mykit.kit.utils import printer
+
 from ..constants import TEMPLATE_DIR_PTH
 
 
 def copying_template(dock):
 
+    LAYOUTS = os.path.join(dock, '_layouts')
+    SASS = os.path.join(dock, '_sass')
+    SCRIPTS = os.path.join(dock, 'scripts')
 
     ## handle the case when the template already exists
-    if os.path.isdir( os.path.join(dock, '_layouts') ):
-        shutil.rmtree( os.path.join(dock, '_layouts') )
-    if os.path.isdir( os.path.join(dock, '_sass') ):
-        shutil.rmtree( os.path.join(dock, '_sass') )
-    if os.path.isdir( os.path.join(dock, 'scripts') ):
-        shutil.rmtree( os.path.join(dock, 'scripts') )
-
+    if os.path.isdir(LAYOUTS): shutil.rmtree(LAYOUTS)
+    if os.path.isdir(SASS): shutil.rmtree(SASS)
+    if os.path.isdir(SCRIPTS): shutil.rmtree(SCRIPTS)
 
     shutil.copytree(
         os.path.join(TEMPLATE_DIR_PTH, 'web', '_layouts'),
-        os.path.join(dock, '_layouts')
+        LAYOUTS
     )
-
     shutil.copytree(
         os.path.join(TEMPLATE_DIR_PTH, 'web', '_sass'),
-        os.path.join(dock, '_sass')
+        SASS
     )
-
     shutil.copytree(
         os.path.join(TEMPLATE_DIR_PTH, 'web', 'scripts'),
-        os.path.join(dock, 'scripts')
+        SCRIPTS
     )
+    printer(f'INFO: Template copied.')
