4a69148d001e23a8f6f8c6dca23af3a104c30aa8
Nicholas Valentinus <nvfastplease@gmail.com>
2023-06-29 14:05:03 -0700

    1.0.0rc8: bug fixed at __main__.py (can't imported during dev)

--- nics/__main__.py
+++ nics/__main__.py
@@ -1,4 +1,7 @@
-from main import main
+try:
+    from nics.main import main
+except ModuleNotFoundError:  # This exception occurred during development ('nics' isn't installed via pip)
+    from main import main
 
 
 if __name__ == '__main__':
