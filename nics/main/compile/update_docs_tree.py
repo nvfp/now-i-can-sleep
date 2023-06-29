e1657565be5b3594d699ab8234b00bc56f0df51f
Nicholas Valentinus <nvfastplease@gmail.com>
2023-06-29 13:46:29 -0700

    tidying update_docs_tree.py

--- nics/main/compile/update_docs_tree.py
+++ nics/main/compile/update_docs_tree.py
@@ -6,12 +6,13 @@ from mykit.kit.utils import printer
 
 
 def update_recursively(dock, D__PAGES, pth, base):
+    printer(f'DEBUG: Updating docs-tree, pth: {repr(pth)}, base: {repr(base)}')
 
     for i in os.listdir(pth):
+        pth2 = os.path.join(pth, i)
 
         ## <handling the index.md>
         if i == 'index.md':
-            src = os.path.join(pth, 'index.md')
             if base == '/':  # homepage
                 dst = os.path.join(dock, 'index.md')
                 text = (
@@ -21,8 +22,6 @@ def update_recursively(dock, D__PAGES, pth, base):
                     'title: Home\n'
                     '---\n\n'
                 )
-                with open(src, 'r') as f: text += f.read()
-                with open(dst, 'w') as f: f.write(text)
             else:
                 dst = os.path.join(D__PAGES, os.sep.join(filter(lambda s:s!='', base.split('/'))), 'index.md')
                 text = (
@@ -32,26 +31,25 @@ def update_recursively(dock, D__PAGES, pth, base):
                     f"title: {list(filter(lambda s:s!='', base.split('/')))[-1]}\n"
                     '---\n\n'
                 )
-                with open(src, 'r') as f: text += f.read()
-                with open(dst, 'w') as f: f.write(text)
+            with open(pth2, 'r') as f: text += f.read()
+            with open(dst, 'w') as f: f.write(text)
+            printer(f'DEBUG: Updated index.md from {repr(pth2)} to {repr(dst)}.')
             continue
         ## </handling the index.md>
 
-        pth2 = os.path.join(pth, i)
-
-        res = re.match(r'(?:\d+ -- )?(?P<name>[\w -.]+) -- (?P<url>[\w -]+)(?:.md)?', i)
+        res = re.match(r'(?:\d+ -- )?(?P<name>[\w -.]+) -- (?P<url>[\w -]+)(?:\.md)?', i)
         name = res.group('name')
         url = res.group('url')
 
         if os.path.isdir(pth2):
-            ## if the dir not exist -> create a new one
-            dir = os.path.join(D__PAGES, os.sep.join(filter(lambda s:s!='', base.split('/'))), name )
+            ## if the folder doesn't exist, create a new one.
+            dir = os.path.join(D__PAGES, os.sep.join(filter(lambda s:s!='', base.split('/'))), name)
             if not os.path.isdir(dir):
-                printer(f'DEBUG: Creating new dir: {repr(dir)}.')
                 os.mkdir(dir)
+                printer(f'DEBUG: Dir created: {repr(dir)}.')
+            ## do it again
             update_recursively(dock, D__PAGES, pth2, base+name+'/')
         else:
-            src = os.path.join(pth, i)
             dst = os.path.join(D__PAGES, os.sep.join(filter(lambda s:s!='', base.split('/'))), f'{name}.md')
             text = (
                 '---\n'
@@ -60,8 +58,9 @@ def update_recursively(dock, D__PAGES, pth, base):
                 f'title: {name}\n'
                 '---\n\n'
             )
-            with open(src, 'r') as f: text += f.read()
+            with open(pth2, 'r') as f: text += f.read()
             with open(dst, 'w') as f: f.write(text)
+            printer(f'DEBUG: Updated docs-tree file from {repr(pth2)} to {repr(dst)}.')
 
 
 def update_docs_tree(dock, C_TREE, D__PAGES):
