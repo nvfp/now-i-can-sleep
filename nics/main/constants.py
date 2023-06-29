8d195f7ce21e261f5a02df853b72968a7b2185c7
Nicholas Valentinus <nvfastplease@gmail.com>
2023-06-29 09:16:11 -0700

    updated ROOT_DIR_PTH in constants.py (because the restructuring)

--- nics/main/constants.py
+++ nics/main/constants.py
@@ -12,7 +12,7 @@ SOFTWARE_REPO = 'https://github.com/nvfp/now-i-can-sleep'
 SOFTWARE_NAME = 'Now I Can Sleep'
 SOFTWARE_DIST_NAME = 'nics'  # distribution name
 
-ROOT_DIR_PTH = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+ROOT_DIR_PTH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DIST_DIR_PTH = os.path.join(ROOT_DIR_PTH, SOFTWARE_DIST_NAME)
 
 TEMPLATE_DIR_PTH = os.path.join(DIST_DIR_PTH, '_template')
