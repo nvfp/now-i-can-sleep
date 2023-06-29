e9db6322be5ac5302a10ed95fd113f036635ea65
Nicholas Valentinus <nvfastplease@gmail.com>
2023-06-28 22:20:26 -0700

    0.28.0: bugfixed: handle the case when the template already exist (do rmtree before copytree)

--- nics/compile/copying_template.py
+++ nics/compile/copying_template.py
@@ -6,6 +6,16 @@ from ..constants import TEMPLATE_DIR_PTH
 
 def copying_template(dock):
 
+
+    ## handle the case when the template already exists
+    if os.path.isdir( os.path.join(dock, '_layouts') ):
+        shutil.rmtree( os.path.join(dock, '_layouts') )
+    if os.path.isdir( os.path.join(dock, '_sass') ):
+        shutil.rmtree( os.path.join(dock, '_sass') )
+    if os.path.isdir( os.path.join(dock, 'scripts') ):
+        shutil.rmtree( os.path.join(dock, 'scripts') )
+
+
     shutil.copytree(
         os.path.join(TEMPLATE_DIR_PTH, '_layouts'),
         os.path.join(dock, '_layouts')
