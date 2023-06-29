30c28c4133a8aae7f4ade61c6cb2749ab824ba97
Nicholas Valentinus <nvfastplease@gmail.com>
2023-06-29 13:21:20 -0700

    tidying the update_footer.py

--- nics/main/compile/update_footer.py
+++ nics/main/compile/update_footer.py
@@ -1,7 +1,7 @@
+from mykit.kit.utils import printer
 
 
-
-def update_footer(footer_pth, show_credit):
+def update_footer(D_FOOTER, show_credit):
 
     text = (
         '<footer>'
@@ -11,5 +11,5 @@ def update_footer(footer_pth, show_credit):
         text += '<p id="credit">built with <a href="https://github.com/nvfp/now-i-can-sleep">now-i-can-sleep</a></p>'
     text += '</footer>'
 
-    with open(footer_pth, 'w') as f:
-        f.write(text)
+    with open(D_FOOTER, 'w') as f: f.write(text)
+    printer(f'INFO: Updated footer {repr(D_FOOTER)}.')
