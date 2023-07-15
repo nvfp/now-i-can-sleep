572bf364d2629f95f7e399c2367bb299d65bc55d
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-15 00:43:50 -0700

    Tweak constants.py

--- nics/main/constants.py
+++ nics/main/constants.py
@@ -4,13 +4,13 @@ import pkg_resources
 
 try:
     __version__ = pkg_resources.get_distribution('nics').version
-except pkg_resources.DistributionNotFound:  # this exception occurred during development (before the software installed via pip)
+except pkg_resources.DistributionNotFound:  # This exception occurred during development (before the software installed via pip)
     __version__ = 'dev'
 
 
 SOFTWARE_REPO = 'https://github.com/nvfp/now-i-can-sleep'
 SOFTWARE_NAME = 'Now I Can Sleep'
-SOFTWARE_DIST_NAME = 'nics'  # distribution name
+SOFTWARE_DIST_NAME = 'nics'  # Distribution name
 
 ROOT_DIR_PTH = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 DIST_DIR_PTH = os.path.join(ROOT_DIR_PTH, SOFTWARE_DIST_NAME)
