d1edb0ad47db1a8e496ba56bbe28f1a66b9d92e3
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-15 06:58:16 -0700

    working on customize/__init__.py

--- nics/main/compile/customize/__init__.py
+++ nics/main/compile/customize/__init__.py
@@ -1,27 +1,25 @@
-
-from nics.main.utils.favicon_maker import create_favicon_svg_file
 from nics.main.compile.customize.custom_config_yml import custom_config_yml
 
 
 def customize_template_with_user_data(container, dock, cfg):
 
-    ## Rewrite nav.html in _includes
+    ## /_includes/nav.html
     rewrite_nav_html()
 
-    ## Rewrite constants.sass in _sass
+    ## /_sass/constants.sass
     rewrite_constants_sass()
 
-    ## 
+    ## /assets/images/favicon.svg or favicon.png
     rewrite_favicon()
 
-    ## Rewrite 404 page in pages/404/index.md
+    ## /pages/404/index.md
     rewrite_404()
 
-    ## Rewrite homepage in pages/home/index.md
+    ## /pages/home/index.md
     rewrite_home()
 
-    ## Rewrite the pages/tree/
+    ## /pages/tree/
     rewrite_docs_tree()
 
-    ## Update _config.yml
+    ## /_config.yml
     custom_config_yml(dock, cfg)
