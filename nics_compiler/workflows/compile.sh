3de9e4a5982bbbe48c0679f8060faa1ea7265b1e
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-18 13:17:22 -0700

    edit compile.sh

--- nics_compiler/workflows/compile.sh
+++ nics_compiler/workflows/compile.sh
@@ -2,14 +2,17 @@
 set -e  # Exit the script immediately if any command exits with a non-zero status
 
 
-echo "::group::Compile"
-
+echo "::group::Preparing"
 
 python -m pip install --upgrade pip
 pip install -r $GITHUB_ACTION_PATH/requirements.txt
 
-cd ..
-python $GITHUB_ACTION_PATH/nics_compiler/compiler $(pwd)
+echo "::endgroup::"
 
+
+echo "::group::Compile"
+
+cd ..
+python $GITHUB_ACTION_PATH/nics_compiler/compiler $(pwd)/$GH_REPO_NAME $(pwd)/__nics_working_dir__
 
 echo "::endgroup::"
