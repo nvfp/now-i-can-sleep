2f8f0cba0d6d5c4e5f4381a542d60605554b635a
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-18 00:01:37 -0700

    renamed debug.sh -> checks.sh

--- nics_compiler/workflows/checks.sh
+++ nics_compiler/workflows/checks.sh
@@ -1,10 +1,16 @@
-echo "::group:: Debug"
+echo "::group::The inputs"
 
 echo "LOAD: '$LOAD'"
 echo "DOCK: '$DOCK'"
+echo "CONTAINER: '$CONTAINER'"
 echo "GIT_NAME: '$GIT_NAME'"
 echo "GIT_EMAIL: '$GIT_EMAIL'"
 echo "GH_REPO_NAME: '$GH_REPO_NAME'"
-echo "CONTAINER: '$CONTAINER'"
+echo "NICS_VERSION: '$NICS_VERSION'"
+
+echo "::endgroup::"
+
+
+echo "::group::The inputs"
 
 echo "::endgroup::"
