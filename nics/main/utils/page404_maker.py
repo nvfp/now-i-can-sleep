fed85a990bbf47bb29b9d3eff831c6d4d37c8b1b
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-18 09:20:03 -0700

    Improved page404_maker.py

--- nics/main/utils/page404_maker.py
+++ nics/main/utils/page404_maker.py
@@ -1,23 +1,22 @@
-import os
+import logging
 
 
-def get_text(cfg):
+logger = logging.getLogger(__name__)
+
+
+def get_text(gh_username, gh_repo_name):
     return f"""
 ## Sorry, it seems the page doesn't exists or no longer exists.
 
-Do want to check it on the [homepage](https://{cfg._gh_username}.github.io/{cfg._gh_repo})?
+Do want to check it on the [homepage](https://{gh_username}.github.io/{gh_repo_name})?
 """
 
 
-def create_404_page_file(pth, cfg):
-    """
-    `pth` is not limited to '404.md' or '/404/index.md'
-    """
+def create_404_page_file(file_path, gh_username, gh_repo_name):
+    logger.debug('Writing 404 page file.')
 
-    ## Check
-    if os.path.exists(pth):
-        raise AssertionError(f'File/dir already exists: {repr(pth)}.')
-
-    text = get_text(cfg)
-    with open(pth, 'w') as f:
+    text = get_text(gh_username, gh_repo_name)
+    with open(file_path, 'w') as f:
         f.write(text)
+    
+    logger.debug(f'404 page created at {repr(file_path)}.')
