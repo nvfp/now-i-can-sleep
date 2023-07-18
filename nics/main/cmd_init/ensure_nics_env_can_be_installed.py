c6ba153f4afa6a92b86d312ed13669a6cc4e56e4
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-18 09:52:07 -0700

    finished ensure_nics_env_can_be_installed.py

--- nics/main/cmd_init/ensure_nics_env_can_be_installed.py
+++ nics/main/cmd_init/ensure_nics_env_can_be_installed.py
@@ -1,5 +1,9 @@
+import logging
 import os
 import sys
+
+
+logger = logging.getLogger(__name__)
 
 
 def ensure_nics_env_can_be_installed(cwd, container):
@@ -7,11 +11,11 @@ def ensure_nics_env_can_be_installed(cwd, container):
     ## /.github/workflows/rebuild-docs.yml
     pth = os.path.join(cwd, '.github', 'workflows', 'rebuild-docs.yml')
     if os.path.isfile(pth):
-        print(f'Cannot initialize NICS. This file already exists: {repr(pth)}.')
+        logger.error(f'Cannot initialize NICS. This file already exists: {repr(pth)}.')
         sys.exit(1)
 
     ## /CONTAINER/
     pth = os.path.join(cwd, container)
     if os.path.isdir(pth):
-        print(f'Cannot initialize NICS. This folder already exists: {repr(pth)}.')
+        logger.error(f'Cannot initialize NICS. This folder already exists: {repr(pth)}.')
         sys.exit(1)
