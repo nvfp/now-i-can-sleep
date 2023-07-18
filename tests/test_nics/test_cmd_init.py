482aae15b8186fe50c7720d604391d3ce03d5897
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-18 07:46:16 -0700

    finished unit test for ensure_nics_env_can_be_installed

--- tests/test_nics/test_cmd_init.py
+++ tests/test_nics/test_cmd_init.py
@@ -4,6 +4,7 @@ from unittest.mock import patch
 import os
 import shutil
 import tempfile
+from io import StringIO
 
 
 from nics.main.cmd_init.ensure_nics_env_can_be_installed import ensure_nics_env_can_be_installed
@@ -20,40 +21,38 @@ class TestEnsureNicsEnvCanBeInstalled(unittest.TestCase):
 
     def test_existing_rebuild_docs_yml(self):
 
+        file_path = os.path.join(self.cwd, '.github', 'workflows', 'rebuild-docs.yml')
+
         ## Create the .github/workflows/ folders
-        os.makedirs(os.path.join(self.cwd, '.github', 'workflows'))
+        os.makedirs(os.path.dirname(file_path))
         ## Create an empty rebuild-docs.yml
-        open(os.path.join(self.cwd, '.github', 'workflows', 'rebuild-docs.yml'), 'w').close()
+        open(file_path, 'w').close()
 
         ## Call the function and check that it exits with an error message
-        with self.assertRaises(SystemExit) as cm:
-            with patch('sys.stdout') as mock_stdout:
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            with self.assertRaises(SystemExit) as cm:
                 ensure_nics_env_can_be_installed(self.cwd, self.container)
         self.assertEqual(cm.exception.code, 1)
-        mock_stdout.assert_called_with(
-            f'Cannot initialize NICS. This file already exists: {repr(os.path.join(self.cwd, ".github", "workflows", "rebuild-docs.yml"))}.'
-        )
+        expected_msg = f'Cannot initialize NICS. This file already exists: {repr(file_path)}.'
+        self.assertEqual(fake_out.getvalue().strip(), expected_msg)
 
     def test_existing_container_dir(self):
 
         ## Create a container directory
-        os.makedirs(os.path.join(self.cwd, self.container))
+        container_dir = os.path.join(self.cwd, self.container)
+        os.makedirs(container_dir)
 
         ## Call the function and check that it exits with an error message
-        with self.assertRaises(SystemExit) as cm:
-            with patch('sys.stdout') as mock_stdout:
+        with patch('sys.stdout', new=StringIO()) as fake_out:
+            with self.assertRaises(SystemExit) as cm:
                 ensure_nics_env_can_be_installed(self.cwd, self.container)
         self.assertEqual(cm.exception.code, 1)
-        mock_stdout.assert_called_with(
-            f'Cannot initialize NICS. This folder already exists: {repr(os.path.join(self.cwd, self.container))}.'
-        )
-    
+        expected_msg = f'Cannot initialize NICS. This folder already exists: {repr(container_dir)}.'
+        self.assertEqual(fake_out.getvalue().strip(), expected_msg)
+
     def test_no_exit(self):
         ## Call the function and check that it does not exit
-        try:
-            ensure_nics_env_can_be_installed(self.cwd, self.container)
-        except SystemExit:
-            self.fail('Function should not have exited')
+        ensure_nics_env_can_be_installed(self.cwd, self.container)
 
 
 # from nics.main.cmd_init.get_user_details import get_user_details
