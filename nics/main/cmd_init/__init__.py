e83bd3bc9f194917d2eb15a096b44e08ab88afba
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-18 08:04:52 -0700

    WIP: finished cmd_init

--- nics/main/cmd_init/__init__.py
+++ nics/main/cmd_init/__init__.py
@@ -1,105 +1,27 @@
 import os
-import shutil
-import sys
 
-from mykit.kit.utils import printer
-
-from nics.main.constants import TEMPLATE_DOCS_DIR_PTH
 from nics.main.utils.ensure_a_git_repo import ensure_a_git_repo
-from nics.main.utils.is_nics_initialized import is_nics_initialized
-from nics.main.cmd_init.workflow_writer import workflow_writer
-from nics.main.cmd_init.settings_writer import settings_writer
-
-
-def outro():
-    text = f"""
-Almost done, now you need to do these final steps:
-1. Create docs branch
-   - git add .
-   - git commit -m "NICS init"
-   - git checkout --orphan docs
-   - git rm -rf .
-   - git commit --allow-empty -m init
-   - git push origin docs
-2. Activate the GitHub Pages
-   - Visit https://github.com/{gh_username}/{gh_repo}/settings/pages
-   - Under 'Build and deployment' section,
-     - For 'Source', select 'Deploy from a branch'
-     - For 'Branch', select 'docs' branch
-     - Click the 'Save' button
-3. Back to {main_branch_name} branch
-   - git checkout {main_branch_name}
-   - git push
-
-That's it! The documentation will be at https://{gh_username}.github.io/{gh_repo} .
-Visit https://nvfp.github.io/now-i-can-sleep/usage/init-command to visually see the final steps above."""
-    print(text)
+from nics.main.cmd_init.ensure_nics_env_can_be_installed import ensure_nics_env_can_be_installed
+from nics.main.cmd_init.get_user_details import get_user_details
+from nics.main.cmd_init.loading import loading
+from nics.main.cmd_init.print_outro import print_outro
 
 
 def run():
 
     CWD = os.getcwd()
 
-    ## Check
+    ## Check I
     ensure_a_git_repo(CWD)
-    ensure_nics_env_can_be_installed(CWD)
 
-    ## Intro
-    print('Welcome to NICS!')
+    ## Get user details
+    usr = get_user_details()
 
-    ## User details
-    author = input('Enter your name: ')
-    email = input('Enter your email: ')
-    gh_username = input('Enter your GitHub username: ')
-    gh_repo = input('Enter this GitHub repository name: ')
-    main_branch_name = input('Enter the main branch name (e.g. master): ')
+    ## Check II
+    ensure_nics_env_can_be_installed(CWD, usr.container)
 
-    ## Case I: Reinitialization (rewrite workflow and setting files only)
-    if is_nics_initialized(CWD):
-        pass
-    else:
-        ## Case II: 
-        if True:
-            pass
-        ## Case III: Fresh Initialization (create workflow file and copy docs/ from template)
-        else:
-            pass
+    ## Loading to the Container
+    loading()
 
-    WORKFLOW_FILE_PTH = os.path.join(CWD, '.github', 'workflows', 'rebuild-docs.yml')
-    CONTAINER_DIR_PTH = os.path.join(CWD, 'docs')
-    SETTINGS_FILE_PTH = os.path.join(CWD, 'docs', 'settings.txt')
-
-    ## already_use is True if the user has used NICS before and only wants to reinitialize; otherwise, False.
-    already_use = False
-    if os.path.isfile(WORKFLOW_FILE_PTH) and os.path.isdir(CONTAINER_DIR_PTH):
-        already_use = True
-
-    if not already_use:
-        if os.path.isfile(WORKFLOW_FILE_PTH) or os.path.isdir(CONTAINER_DIR_PTH):
-            printer(f'ERROR: Workflow file or docs/ folder already exist.')
-            sys.exit(1)
-
-    if already_use:
-        print(
-            'Workflow file and docs/ folder are found, will rewrite these following files:\n'
-            f'- Workflow file {repr(WORKFLOW_FILE_PTH)}\n'
-            f'- Settings file {repr(SETTINGS_FILE_PTH)}'
-        )
-        usr = input('Continue?  (Y/n): ')
-        if usr not in ['', 'y', 'Y']:
-            printer(f'INFO: Exited.')
-            sys.exit(0)
-
-
-
-    workflow_writer(WORKFLOW_FILE_PTH, author, email, gh_repo, main_branch_name)
-
-    if not already_use:
-        printer(f"INFO: Copying 'docs/' folder.")
-        shutil.copytree(TEMPLATE_DOCS_DIR_PTH, CONTAINER_DIR_PTH)
-        printer(f'INFO: Done, {repr(CONTAINER_DIR_PTH)} is created.')
-
-    settings_writer(SETTINGS_FILE_PTH, author, email, gh_username, gh_repo, main_branch_name)
-
-    ## outro
-    outro()
+    ## Outro
+    print_outro()
