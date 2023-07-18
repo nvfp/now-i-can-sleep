1b84d188260fa00931d188439b0028f9dd9be11c
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-18 09:51:00 -0700

    finished print_outro.py

--- nics/main/cmd_init/print_outro.py
+++ nics/main/cmd_init/print_outro.py
@@ -1,25 +1,24 @@
 
 
-def print_outro():
+def print_outro(load, dock, gh_username, gh_repo_name):
     text = f"""
 Almost done, now you need to do these final steps:
-1. Create docs branch
+1. Create {dock} branch
    - git add .
    - git commit -m "NICS init"
-   - git checkout --orphan docs
+   - git checkout --orphan {dock}
    - git rm -rf .
    - git commit --allow-empty -m init
-   - git push origin docs
+   - git push origin {dock}
 2. Activate the GitHub Pages
-   - Visit https://github.com/{gh_username}/{gh_repo}/settings/pages
+   - Visit https://github.com/{gh_username}/{gh_repo_name}/settings/pages
    - Under 'Build and deployment' section,
      - For 'Source', select 'Deploy from a branch'
-     - For 'Branch', select 'docs' branch
+     - For 'Branch', select '{dock}' branch
      - Click the 'Save' button
-3. Back to {main_branch_name} branch
-   - git checkout {main_branch_name}
+3. Back to {load} branch
+   - git checkout {load}
    - git push
 
-That's it! The documentation will be at https://{gh_username}.github.io/{gh_repo} .
-Visit https://nvfp.github.io/now-i-can-sleep/usage/init-command to visually see the final steps above."""
+That's it! The documentation will be at https://{gh_username}.github.io/{gh_repo_name} ."""
     print(text)
