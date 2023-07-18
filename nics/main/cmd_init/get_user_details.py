942bd2087d344a3679689b6f1157a3ad8e8cd0d1
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-18 08:05:07 -0700

    added prototype get_user_details

--- nics/main/cmd_init/get_user_details.py
+++ nics/main/cmd_init/get_user_details.py
@@ -1,7 +1,17 @@
 
 
 def get_user_details():
-    
-    details = {}
 
-    name = input('Enter your Git username: ')
+    ## Intro
+    print('Welcome to NICS!')
+    
+    class usr: ...
+
+    usr.name = input('Enter your Git username: ')
+    usr.author = input('Enter your name: ')
+    usr.email = input('Enter your email: ')
+    usr.gh_username = input('Enter your GitHub username: ')
+    usr.gh_repo = input('Enter this GitHub repository name: ')
+    usr.main_branch_name = input('Enter the main branch name (e.g. master): ')
+
+    return usr
