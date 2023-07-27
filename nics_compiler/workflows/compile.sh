e7f79cc0dd2a61be84407bc3346d91233dbc997f
Nicholas Valentinus <nvfastplease@gmail.com>
2023-07-27 08:25:09 -0700

    3.0.0b13 Testing: test compiler docking

--- nics_compiler/workflows/compile.sh
+++ nics_compiler/workflows/compile.sh
@@ -13,6 +13,6 @@ echo "::endgroup::"
 echo "::group::Compile"
 
 echo "DEBUG: pwd: '$(pwd)'"
-python $GITHUB_ACTION_PATH/nics_compiler/compiler $(pwd)/$GH_REPO_NAME $(pwd)/__nics_work_dir__
+python $GITHUB_ACTION_PATH/nics_compiler/compiler $(pwd)/../$GH_REPO_NAME $(pwd)/../__nics_work_dir__
 
 echo "::endgroup::"
