0f822d225bb73b360e4ec7a51f7a52f70cd8353d
Nicholas Valentinus <nvfastplease@gmail.com>
2023-10-15 23:34:54 -0700

    this is a dev commit: 2 files changed, 4 insertions(+), 2 deletions(-)
     action.yml       | 2 +-
    
     nics/__main__.py | 4 +++-
    
    message generated by QWD (github.com/nvfp/qwd)

--- nics/__main__.py
+++ nics/__main__.py
@@ -1,2 +1,4 @@
-from . import main
+import os,sys;sys.path.append(os.environ['GITHUB_ACTION_PATH'])
+print(os.listdir(os.environ['GITHUB_ACTION_PATH']))
+from nics import main
 if __name__=='__main__':main()
