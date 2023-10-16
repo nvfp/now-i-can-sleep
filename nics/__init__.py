297f22ec179b3f0119477bd80a1ffa09f0ac7b5a
Nicholas Valentinus <nvfastplease@gmail.com>
2023-10-16 00:59:02 -0700

    this is a dev commit: 1 file changed, 3 insertions(+), 1 deletion(-)
     nics/__init__.py | 4 +++-
    
    message generated by QWD (github.com/nvfp/qwd)

--- nics/__init__.py
+++ nics/__init__.py
@@ -3,11 +3,13 @@ import os
 from .core import engine
 
 
-NICS_DIR = os.environ['NICS_DIR']
+NICS_DIR = os.path.join(os.environ['GITHUB_WORKSPACE'], os.path.abspath(os.path.normpath(os.environ['NICS_DIR'])))
 
 
 def main():
 
     print(NICS_DIR)
+    print('---')
+    print(os.listdir(NICS_DIR))
 
     engine()
