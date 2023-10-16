6cddf9f3c4bef429ea7a5cb3c972e8899ab23fe0
Nicholas Valentinus <nvfastplease@gmail.com>
2023-10-16 00:56:42 -0700

    this is a dev commit: 3 files changed, 10 insertions(+), 2 deletions(-)
     action.yml       | 2 ++
    
     nics/__init__.py | 9 ++++++++-
    
     nics/__main__.py | 1 -
    
    message generated by QWD (github.com/nvfp/qwd)

--- nics/__init__.py
+++ nics/__init__.py
@@ -1,6 +1,13 @@
+import os
+
 from .core import engine
 
 
+NICS_DIR = os.environ['NICS_DIR']
+
+
 def main():
-    print('test main')
+
+    print(NICS_DIR)
+
     engine()
--- nics/__main__.py
+++ nics/__main__.py
@@ -1,4 +1,3 @@
 import os,sys;sys.path.append(os.environ['GITHUB_ACTION_PATH'])
-print(os.listdir(os.environ['GITHUB_ACTION_PATH']))
 from nics import main
 if __name__=='__main__':main()
