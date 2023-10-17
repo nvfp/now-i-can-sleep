c84a18c8be5b01a649e5e802ed39dccca21fe69e
Nicholas Valentinus <nvfastplease@gmail.com>
2023-10-16 22:54:41 -0700

    finished 3.0.0 prototype

--- nics/core/validations/test.py
+++ nics/core/validations/test.py
@@ -13,30 +13,17 @@ def nics__dir_should_be_a_valid_dir():
     tt.both_are_equal(its.exception_msg, 'not a dir')
 
 
-def if_settings_not_found():
-
-    TD = tempfile.mkdtemp()
-    DIR = os.path.join(TD, 'nics_dir')
-
-    os.mkdir(os.path.join(DIR, 'pages'))
-    open(os.path.join(DIR, 'favicon.ico'), 'w').close()
-    tt.both_are_equal(sorted(os.listdir(DIR)), ['favicon.ico', 'pages'])
-
-    with tt.these_will_raise(AssertionError) as its: validations(DIR)
-    tt.both_are_equal(its.exception_msg, "The folder should only contain favicon.ico, pages, and settings.txt")
-
-
 def if_favicon_not_found():
 
     TD = tempfile.mkdtemp()
     DIR = os.path.join(TD, 'nics_dir')
 
     os.mkdir(os.path.join(DIR, 'pages'))
-    open(os.path.join(DIR, 'settings.txt'), 'w').close()
-    tt.both_are_equal(sorted(os.listdir(DIR)), ['pages', 'settings.txt'])
+    open(os.path.join(DIR, 'foo-bar.txt'), 'w').close()
+    tt.both_are_equal(sorted(os.listdir(DIR)), ['foo-bar.txt', 'pages'])
 
     with tt.these_will_raise(AssertionError) as its: validations(DIR)
-    tt.both_are_equal(its.exception_msg, "The folder should only contain favicon.ico, pages, and settings.txt")
+    tt.both_are_equal(its.exception_msg, "favicon.ico not found")
 
 
 def if_pages_dir_not_found():
@@ -45,11 +32,11 @@ def if_pages_dir_not_found():
     DIR = os.path.join(TD, 'nics_dir')
 
     open(os.path.join(DIR, 'favicon.ico'), 'w').close()
-    open(os.path.join(DIR, 'settings.txt'), 'w').close()
-    tt.both_are_equal(sorted(os.listdir(DIR)), ['favicon.ico', 'settings.txt'])
+    open(os.path.join(DIR, 'foo-bar.txt'), 'w').close()
+    tt.both_are_equal(sorted(os.listdir(DIR)), ['favicon.ico', 'foo-bar.txt'])
 
     with tt.these_will_raise(AssertionError) as its: validations(DIR)
-    tt.both_are_equal(its.exception_msg, "The folder should only contain favicon.ico, pages, and settings.txt")
+    tt.both_are_equal(its.exception_msg, "pages/ not found")
 
 
 def if_pages_dir_empty():
@@ -59,41 +46,39 @@ def if_pages_dir_empty():
 
     os.mkdir(os.path.join(DIR, 'pages'))
     open(os.path.join(DIR, 'favicon.ico'), 'w').close()
-    open(os.path.join(DIR, 'settings.txt'), 'w').close()
+    open(os.path.join(DIR, 'foobar.json'), 'w').close()
 
     with tt.these_will_raise(AssertionError) as its: validations(DIR)
     tt.both_are_equal(its.exception_msg, "pages/ folder shouldn't be empty")
 
 
-def if_nonfile_found_in_pages_dir():
+def if_nonmarkdown_file_found_in_pages_dir():
 
     TD = tempfile.mkdtemp()
     DIR = os.path.join(TD, 'nics_dir')
 
     os.mkdir(os.path.join(DIR, 'pages'))
     open(os.path.join(DIR, 'favicon.ico'), 'w').close()
-    open(os.path.join(DIR, 'settings.txt'), 'w').close()
-
-    os.mkdir(os.path.join(DIR, 'pages', 'foo'))
-    open(os.path.join(DIR, 'pages', 'about.md'), 'w').close()
-    tt.both_are_equal(sorted(os.listdir(os.path.join(DIR, 'pages'))), ['about.md', 'foo'])
-
-    with tt.these_will_raise(AssertionError) as its: validations(DIR)
-    tt.both_are_equal(its.exception_msg, "This 'foo' is not a file")
-
-
-def if_nonmarkdown_found_in_pages_dir():
-
-    TD = tempfile.mkdtemp()
-    DIR = os.path.join(TD, 'nics_dir')
-
-    os.mkdir(os.path.join(DIR, 'pages'))
-    open(os.path.join(DIR, 'favicon.ico'), 'w').close()
-    open(os.path.join(DIR, 'settings.txt'), 'w').close()
 
     open(os.path.join(DIR, 'pages', 'about.md'), 'w').close()
     open(os.path.join(DIR, 'pages', 'foo.txt'), 'w').close()
     tt.both_are_equal(sorted(os.listdir(os.path.join(DIR, 'pages'))), ['about.md', 'foo.txt'])
 
     with tt.these_will_raise(AssertionError) as its: validations(DIR)
-    tt.both_are_equal(its.exception_msg, "This 'foo.txt' is not a markdown")
+    tt.both_are_equal(its.exception_msg, "Only Markdown files allowed in the `pages/` folder.")
+
+
+def if_a_dir_found_in_pages_dir():
+
+    TD = tempfile.mkdtemp()
+    DIR = os.path.join(TD, 'nics_dir')
+
+    os.mkdir(os.path.join(DIR, 'pages'))
+    open(os.path.join(DIR, 'favicon.ico'), 'w').close()
+
+    open(os.path.join(DIR, 'pages', 'about.md'), 'w').close()
+    os.mkdir(os.path.join(DIR, 'pages', 'foobar'))
+    tt.both_are_equal(sorted(os.listdir(os.path.join(DIR, 'pages'))), ['about.md', 'foobar'])
+
+    with tt.these_will_raise(AssertionError) as its: validations(DIR)
+    tt.both_are_equal(its.exception_msg, "Only Markdown files allowed in the `pages/` folder.")
