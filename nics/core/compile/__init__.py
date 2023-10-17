9a8936717a363c78e16346299bf2c221dedbca02
Nicholas Valentinus <nvfastplease@gmail.com>
2023-10-16 23:56:26 -0700

    this is a dev commit: 1 file changed, 1 insertion(+)
     nics/core/compile/__init__.py | 1 +
    
    message generated by QWD (github.com/nvfp/qwd)

--- nics/core/compile/__init__.py
+++ nics/core/compile/__init__.py
@@ -24,6 +24,7 @@ def prepare():
     
     ## Copy the template
     shutil.move(TEMPLATE_DIR, CWD)
+    print(f"DEBUG: os.listdir(CWD) after copying template:{os.listdir(CWD)}")
 
     ## Remove the files that will be replaced soon
     os.remove(os.path.join(CWD, '_config.yml'))
